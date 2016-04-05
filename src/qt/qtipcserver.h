ea740ff799025cdd27dfdd06fe8076a2aa7b550c
Tillkoeln <Tillkoeln@Tillkoeln.com>
2016-04-06 01:33:17 +0200

    Basic Source

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Adderalcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AdderalcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
